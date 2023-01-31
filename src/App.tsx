dd2af9aaea39d1e485038f8b3db188064f0e6dc2
Berador Stambekov <stambekovbera@gmail.com>
2023-01-31 22:43:13 +0500

    create routes & create chunks

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import { Counter } from "./components/Counter";
+import React, { Suspense } from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import './index.scss';
 
 export const App = () => {
 	return (
 		<div className="wrapper">
-			<Counter/>
+			<Link to='/'>Главная</Link>
+			<Link to='/about'>О сайте</Link>
+			<Suspense fallback={ <div>Loading...</div> }>
+				<Routes>
+					<Route path='/' element={ <MainPageAsync/> }/>
+					<Route path='/about' element={ <AboutPageAsync/> }/>
+				</Routes>
+			</Suspense>
 		</div>
 	);
 };
