0e982d851bc3ebc7f64b15d5bcfafe69b267d1a0
Berador Stambekov <stambekovbera@gmail.com>
2023-02-25 02:45:42 +0500

    add stories for all shared component

---
+++ src/shared/ui/AppLink/AppLink.stories.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import { AppLink, AppLinkTheme } from './AppLink';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+
+export default {
+    title: 'shared/AppLink',
+    component: AppLink,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+    args: {
+        to: '/',
+    }
+} as ComponentMeta<typeof AppLink>;
+
+const Template: ComponentStory<typeof AppLink> = (args) => <AppLink {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+    children: 'Text',
+    theme: AppLinkTheme.PRIMARY,
+};
+
+export const PrimaryDark = Template.bind({});
+PrimaryDark.args = {
+    children: 'Text',
+    theme: AppLinkTheme.PRIMARY,
+};
+PrimaryDark.decorators = [ ThemeDecorator( Theme.DARK ) ];
+
+export const Inverted = Template.bind({});
+Inverted.args = {
+    children: 'Text',
+    theme: AppLinkTheme.INVERTED,
+};
+
+export const InvertedDark = Template.bind( {} );
+InvertedDark.args = {
+    children: 'Text',
+    theme: AppLinkTheme.INVERTED,
+};
+InvertedDark.decorators = [ ThemeDecorator( Theme.DARK ) ];
