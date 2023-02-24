da796f2bf1fd32cd2c5a14ae6a854904b5a67873
Berador Stambekov <stambekovbera@gmail.com>
2023-02-25 02:29:50 +0500

    add stories for all widgets component

--- src/widgets/PageError/ui/PageError.tsx
+++ src/widgets/PageError/ui/PageError.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import classes from './PageError.module.scss';
 import { useTranslation } from 'react-i18next';
-import { Button } from 'shared/ui/Button/Button';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 
 interface IPageErrorProps {
     className?: string;
@@ -20,10 +20,10 @@ export const PageError: React.FC<IPageErrorProps> = (props) => {
     };
 
     return (
-        <div className={ classNames(classes.pageError, {}, [ className ]) }>
-            <p>{ t('page_error_message') }</p>
-            <Button onClick={ reloadPage }>
-                { t('page_reload_text') }
+        <div className={ classNames( classes.pageError, {}, [ className ] ) }>
+            <p>{ t( 'page_error_message' ) }</p>
+            <Button theme={ ButtonTheme.OUTLINED } onClick={ reloadPage }>
+                { t( 'page_reload_text' ) }
             </Button>
         </div>
     );
