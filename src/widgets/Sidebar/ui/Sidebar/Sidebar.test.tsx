d7a889d1631cc95929a43803f21b2bc6f70051a9
Berador Stambekov <stambekovbera@gmail.com>
2023-03-05 18:44:42 +0500

    fix router test

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,15 +1,15 @@
 import { fireEvent, screen } from '@testing-library/react';
 import { Sidebar } from 'widgets/Sidebar/ui/Sidebar/Sidebar';
-import { renderWithTranslation } from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
 
 describe('Sidebar tests', () => {
     test('sidebar to be in the document', () => {
-        renderWithTranslation(<Sidebar/>);
+        componentRender( <Sidebar/> );
         expect(screen.getByTestId('sidebar-testId')).toBeInTheDocument();
     });
 
     test('sidebar toggle test', () => {
-        renderWithTranslation(<Sidebar/>);
+        componentRender( <Sidebar/> );
         const sidebarToggleBtn = screen.getByTestId('sidebar-toggle-btn-testId');
         fireEvent.click(sidebarToggleBtn);
         expect(screen.getByTestId('sidebar-testId')).toHaveClass('collapsed');
