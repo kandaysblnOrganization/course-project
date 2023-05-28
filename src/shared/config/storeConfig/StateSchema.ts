d3286534c1a33ded77e9cab85c0f47810ed0bbbd
Berador Stambekov <stambekovbera@gmail.com>
2023-05-28 21:58:21 +0500

    add reducer manager

--- src/shared/config/storeConfig/StateSchema.ts
+++ src/shared/config/storeConfig/StateSchema.ts
@@ -1,9 +1,25 @@
 import { ICounterSchema } from 'entities/Counter';
 import { IUserSchema } from 'entities/User';
 import { ILoginSchema } from 'features/AuthByUsername';
+import { AnyAction, CombinedState, EnhancedStore, Reducer, ReducersMapObject } from '@reduxjs/toolkit';
 
 export interface IStateSchema {
     counter: ICounterSchema;
     user: IUserSchema;
-    loginForm: ILoginSchema;
+
+    // ASYNC REDUCERS
+    loginForm?: ILoginSchema;
+}
+
+export type StateSchemaKey = keyof IStateSchema;
+
+export interface IReducerManager {
+    getReducerMap: () => ReducersMapObject<IStateSchema>
+    reduce: (state: IStateSchema, action: AnyAction) => CombinedState<IStateSchema>
+    add: (key: StateSchemaKey, reducer: Reducer) => void
+    remove: (key: StateSchemaKey) => void
+}
+
+export interface IStoreWithManager extends EnhancedStore<IStateSchema> {
+    reducerManage: string;
 }
--- src/shared/config/storeConfig/store.ts
+++ src/shared/config/storeConfig/store.ts
@@ -1,8 +1,9 @@
 import { configureStore, ReducersMapObject } from '@reduxjs/toolkit';
-import { IStateSchema } from './StateSchema';
+import { IStateSchema, IStoreWithManager } from './StateSchema';
 import { counterReducer } from 'entities/Counter';
 import { userReducer } from 'entities/User';
 import { loginReducer } from 'features/AuthByUsername';
+import { createReducerManager } from './reducerManager';
 
 export const createReduxStore = (initialState?: IStateSchema) => {
     const rootReducers: ReducersMapObject<IStateSchema> = {
@@ -11,9 +12,16 @@ export const createReduxStore = (initialState?: IStateSchema) => {
         loginForm: loginReducer
     };
 
-    return configureStore<IStateSchema>({
+    const reducerManager = createReducerManager(rootReducers);
+
+    const store = configureStore<IStateSchema>({
         reducer: rootReducers,
         devTools: __IS_DEV__,
         preloadedState: initialState,
     });
+
+    // // @ts-ignore
+    // store.reducerManager = reducerManager;
+
+    return store;
 };
